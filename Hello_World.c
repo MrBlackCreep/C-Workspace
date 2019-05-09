f159cf909eedc50e5e640247cc67f8fde8a9d714
Maximilian <maximilian-oliwa@web.de>
2019-05-09 14:54:55 +0200

    Assigment2

--- Hello_World.c
+++ Hello_World.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <time.h>   	// for clock_t, clock(), CLOCKS_PER_SEC
+#include <unistd.h> 	// for sleep()
 
 
 int main() {
-    double x = 0.0000005;
-    double C = 0.00000000000000009 + 1;
-    printf("%.17g \n", C);
+    long double x = pow(10L,20L);
+    
+    printf("%lf \n", x);
+    printf("%lf \n", x);
+    printf("%lf \n", pow(x,0.75));
+    printf("%lf \n", pow(x+1, 0.75)-pow(x,0.75));
+	return 0;
 }
